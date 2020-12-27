1224a7f0bdd3f2edc44b14816a64f21e61fe19a7
Yang09701194 <mustnecessity@gmail.com>
2020-12-27 19:12:28 +0800

    20201227

---
+++ src/test.java
@@ -0,0 +1,12 @@
+public class test {
+
+    public static void main (String[] args){
+
+        HashTest.HashTestFunc();
+
+
+        System.out.println("");
+        //System.out.println("hello world!");
+
+    }
+}

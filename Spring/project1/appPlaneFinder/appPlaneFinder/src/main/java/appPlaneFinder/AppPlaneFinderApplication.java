e98bef5422eab8e32c5a83cfa61a936c49a11812
Yang09701194 <mustnecessity@gmail.com>
2022-09-27 16:07:57 +0800

    planefinder

---
+++ Spring/project1/appPlaneFinder/appPlaneFinder/src/main/java/appPlaneFinder/AppPlaneFinderApplication.java
@@ -0,0 +1,13 @@
+package appPlaneFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPlaneFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppPlaneFinderApplication.class, args);
+	}
+
+}

04f6d7817a6bf697c7186d7f0ca60b16b196551b
Yang09701194 <mustnecessity@gmail.com>
2022-09-27 16:14:21 +0800

    Bean

---
+++ Spring/project1/appPlaneFinder/appPlaneFinder/src/main/java/appPlaneFinder/PositionReporter.java
@@ -0,0 +1,30 @@
+package appPlaneFinder;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+import java.security.Signature;
+import java.util.List;
+import java.util.function.Supplier;
+
+@AllArgsConstructor
+@Configuration
+public class PositionReporter {
+
+    private final PlaneFinderService service;
+
+    @Bean
+    Supplier<Iterable<Aircraft>> reportPositions(){
+        return ()->{
+            try {
+                return service.getAircraft();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return List.of();
+        };
+    }
+
+}

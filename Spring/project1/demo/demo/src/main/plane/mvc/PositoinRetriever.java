f85a574353126dd29783cd87b8f1501f56946fc4
Yang09701194 <mustnecessity@gmail.com>
2022-09-28 17:50:49 +0800

    websocket

--- Spring/project1/demo/demo/src/main/plane/mvc/PositoinRetriever.java
+++ Spring/project1/demo/demo/src/main/plane/mvc/PositoinRetriever.java
@@ -4,7 +4,10 @@ package main.plane.mvc;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -12,6 +15,7 @@ import java.util.function.Consumer;
 @Configuration
 public class PositoinRetriever {
     private final AircraftRepository repository;
+    private final WebSocketHandler handler;
 
     @Bean
     Consumer<List<Aircraft>> retrieveAircraftPositions() {
@@ -21,7 +25,24 @@ public class PositoinRetriever {
             repository.saveAll(acList);
 
             repository.findAll().forEach(System.out::println);
+
+            sendPositions();
         };
     }
+
+    private void sendPositions(){
+        if(repository.count() > 0){
+            for(WebSocketSession sessionInList: handler.getSessionList()){
+                try {
+                    sessionInList.sendMessage(
+                            new TextMessage(repository.findAll().toString())
+                    );
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 
 }
